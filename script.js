f352a7b075a1201226ff725eb350a2f3069535cb
Brian Short <brian.ralph.short@gmail.com>
2020-10-18 03:53:02 -0400

    Review/rating section autopopulates from local storage; add clear button to clear local storage and empty section

--- script.js
+++ script.js
@@ -36,86 +36,58 @@ $.ajax({
 });
 
 
+//Brian's Functions
 
-if (!localStorage.getItem("counter")) {
-  var counter = 0;
+
+var entries;
+
+if (!localStorage.getItem("entries")) {
+  $("#journal").html("");
+  entries = [];
 } else {
-  var counter = parseInt(localStorage.getItem("counter"));
-  for (var j = counter - 1; j >= 0; j--) {
-      var newCounter = j;
+  entries = JSON.parse(localStorage.getItem("entries"));
+  console.log(entries);
+  for (var j = entries.length - 1; j > -1; j--) {
       let startupDiv = $("<div>");
-      /*var gotItem = JSON.stringify(localStorage.getItem(`media-item${newCounter}`));
-      var gotReview = JSON.stringify(localStorage.getItem(`review${newCounter}`));
-      var gotRating = JSON.stringify(localStorage.getItem(`rating${newCounter}`));
-      startupDiv.html(`<strong>${gotItem}</strong>: <em>${gotReview}</em><br> ${gotRating} <i class="far fa-star"></i><hr>`);
-      $("#journal").prepend(startupDiv);*/
+      startupDiv.html(`<strong>${entries[j].item}</strong>: <em>${entries[j].review}</em><br>
+        ${entries[j].rating} <i class="far fa-star"></i><hr>`);
+      $("#journal").append(startupDiv);
   }
 }
 //console.log(counter);
 
-var mediaItems = [];
-var reviews = [];
-var ratings = [];
-var entries = [];
-
-//Brian's Functions
-$(".journal_btn").click(function(){
-  counter++;  
+$(".journal_btn").click(function(event){
+  event.preventDefault();
   var mediaItem = $("#item-input").val();
   var review = $("#review-area").val();
   var rating = $('input[name=stars]:checked').val();
-  const entry = {
-    counter: counter,
+  entry = {
     item: mediaItem,
     review: review,
     rating: rating
   }
   entries.push(entry);
-  //console.log(entry);
-  //console.log(entries);
+  console.log(entries);
   localStorage.setItem("entries" , JSON.stringify(entries));
-  /*localStorage.setItem(`media-item${counter}` , mediaItem);
-  localStorage.setItem(`review${counter}` , review)
-  localStorage.setItem(`rating${counter}` , rating)*/
   appendJournal();
   })
 
   function appendJournal() {
-    var entryArray = JSON.parse(localStorage.getItem("entries"));
-    console.log(entryArray)
-    for (var i = entryArray.length - 1; i >= 0; i--) {
-      let startupDiv = $("<div>");
-      startupDiv.html(`<strong>${entryArray[i].item}</strong>: <em>${entryArray[i].review}</em><br>
-        ${entryArray[i].rating} <i class="far fa-star"></i><hr>`);
-        $("#journal").append(startupDiv);
+    $("#journal").html("");
+    entries = JSON.parse(localStorage.getItem("entries"));
+    console.log(entries)
+    for (var i = entries.length - 1; i >= 0; i--) {
+      let appendDiv = $("<div>");
+      appendDiv.html(`<strong>${entries[i].item}</strong>: <em>${entries[i].review}</em><br>
+        ${entries[i].rating} <i class="far fa-star"></i><hr>`);
+        $("#journal").append(appendDiv);
       }
-      
-      /*var newItem = JSON.stringify(localStorage.getItem(`media-item${newIndex}`));
-      var newReview = JSON.stringify(localStorage.getItem(`review${newIndex}`));
-      var newRating = JSON.stringify(localStorage.getItem(`rating${newIndex}`));
-      startupDiv.html(`<strong>${newItem}</strong>: <em>${newReview}</em><br> ${newRating} <i class="far fa-star"></i><hr>`);*/
-     
-    
-    /*$("#journal").html("");
-    for (var i = 0; i < mediaItems.length; i++) {
-      let journalDiv = $("<div>");
-      journalDiv.html(`<strong>${mediaItems[i]}</strong>: <em>${reviews[i]}</em><br>${ratings[i]} <i class="far fa-star"></i><hr>`);
-      $("#journal").prepend(journalDiv);
-      }*/
   }
 
-})
+  $("#clear-click").on("click" , function() {
+    entries = [];
+    localStorage.setItem("entries" , JSON.stringify(entries));
+    $("#journal").html("");
+  })
 
-/*        if (query_count < 10) {
-            query_count++;
-            let queryBreak = $("<hr>");
-            let queryEl = $("<div>");
-            queryEl.text(query_param);
-            queryEl.attr("data-name" , query_param);
-            $(".search-box").append(queryBreak , queryEl);
-            queryEl.on("click" , function() {
-                var searchCity = $(this).attr("data-name");
-                    $("#city").html(searchCity);
-                    getCoordinates(searchCity);
-                })
-            }*/
+})
